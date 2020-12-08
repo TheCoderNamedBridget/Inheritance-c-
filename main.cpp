2b63d1fe36c5a65a9c5c19fbaec4d3945cf5fff4
Bridget Naylor <48873843+TheCoderNamedBridget@users.noreply.github.com>
2020-12-07 16:24:59 -0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,26 @@
+/*
+Bridget Naylor
+Date:12/5/2020
+Class: CECS 282 Section 04
+Lab: 6
+Honesty statement:
+I certify that this program is my own original work. I did not copy any part of this program from
+any other source. I further certify that I typed each and every line of code in this program.
+*/
+#include <stdio.h>
+
+
+//the program will ask for the number of decks, and the number of pl
+int main()
+{
+    int numDecks;
+    int numPlayers;
+    cout<<"Enter number of decks: ";
+    cin>>numDecks;
+    
+    cout<<"Enter number of players: ";
+    cin>>numPlayers;
+
+    return 0;
+}
+
